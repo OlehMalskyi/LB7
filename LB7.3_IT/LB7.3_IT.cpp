86a50d6d97034499d32b3274216f0b06a801666c
OlehMalskyi <omalskij3@gmail.com>
2023-11-09 19:29:49 +0200

    lb7.3_ir

---
+++ LB7.3_IT/LB7.3_IT.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <Windows.h>
+
+using namespace std;
+
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High)
+{
+    for (int i = 0; i < rowCount; i++)
+        for (int j = 0; j < colCount; j++)
+            a[i][j] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int** a, const int rowCount, const int colCount)
+{
+    cout << endl;
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+            cout << setw(4) << a[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void Change(int** a, int colCount, int row1, int row2)
+{
+    for (int i = 0; i < colCount; ++i)
+    {
+        int temp = a[row1][i];
+        a[row1][i] = a[row2][i];
+        a[row2][i] = temp;
+    }
+}
+
+int firstZeroColumn(int** a, int rowCount, int colCount)
+{
+    for (int i = 0; i < colCount; i++)
+    {
+        for (int j = 0; j < rowCount; j++)
+        {
+            if (a[j][i] == 0)
+                return i;
+        }
+    }
+    return -1;
+}
+
+int rowSum(int** a, int row, int colCount)
+{
+    int sum = 0;
+    for (int i = 0; i < colCount; i++)
+    {
+        if (a[row][i] < 0 && a[row][i] % 2 == 0)
+        {
+            sum += a[row][i];
+        }
+    }
+    return sum;
+}
+
+void Sort(int** a, int rowCount, int colCount)
+{
+    for (int j = 0; j < rowCount - 1; j++)
+        for (int i = 0; i < rowCount - j - 1; i++)
+        {
+            if (rowSum(a, i, colCount) < rowSum(a, i + 1, colCount))
+            {
+                Change(a, colCount, i, i + 1);
+            }
+        }
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int Low = -10;
+    int High = 10;
+    int rowCount;
+    int colCount;
+    cout << "k = ";
+    cin >> rowCount;
+    cout << "n = ";
+    cin >> colCount;
+
+    int** a = new int* [rowCount];
+
+    for (int i = 0; i < rowCount; i++)
+        a[i] = new int[colCount];
+
+    Create(a, rowCount, colCount, Low, High);
+    Print(a, rowCount, colCount);
+
+    int zeroColumn = firstZeroColumn(a, rowCount, colCount);
+    if (zeroColumn != -1)
+        cout << "Перший 0 в стовпці: " << zeroColumn + 1 << endl;
+    else
+        cout << "Нульових елементів у матриці немає" << endl;
+
+    Sort(a, rowCount, colCount);
+    Print(a, rowCount, colCount);
+
+    for (int i = 0; i < rowCount; i++)
+        delete[] a[i];
+    delete[] a;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,82 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB7.3_IT/LB7.3_IT.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestFirstZeroColumn)
+		{
+			const int rowCount = 3;
+			const int colCount = 3;
+			int** a = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				a[i] = new int[colCount];
+
+
+			a[0][0] = 1; a[0][1] = 0; a[0][2] = 3;
+			a[1][0] = 4; a[1][1] = 5; a[1][2] = 6;
+			a[2][0] = 7; a[2][1] = 8; a[2][2] = 9;
+
+			int result = firstZeroColumn(a, rowCount, colCount);
+
+			Assert::AreEqual(1, result);
+
+			for (int i = 0; i < rowCount; i++)
+				delete[] a[i];
+			delete[] a;
+		}
+
+		TEST_METHOD(TestRowSum)
+		{
+			const int rowCount = 3;
+			const int colCount = 3;
+			int** a = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				a[i] = new int[colCount];
+
+
+			a[0][0] = -2; a[0][1] = 4; a[0][2] = -6;
+			a[1][0] = 1; a[1][1] = 3; a[1][2] = 5;
+			a[2][0] = -8; a[2][1] = 6; a[2][2] = -4;
+
+			int result = rowSum(a, 0, colCount);
+
+			Assert::AreEqual(-8, result);
+
+			for (int i = 0; i < rowCount; i++)
+				delete[] a[i];
+			delete[] a;
+		}
+
+		TEST_METHOD(TestSort)
+		{
+			const int rowCount = 3;
+			const int colCount = 3;
+			int** a = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				a[i] = new int[colCount];
+
+
+			a[0][0] = 1; a[0][1] = 2; a[0][2] = 3;
+			a[1][0] = -2; a[1][1] = 4; a[1][2] = -6;
+			a[2][0] = 7; a[2][1] = 8; a[2][2] = 9;
+
+			Sort(a, rowCount, colCount);
+
+
+			Assert::AreEqual(0, rowSum(a, 0, colCount));
+			Assert::AreEqual(0, rowSum(a, 1, colCount));
+			Assert::AreEqual(-8, rowSum(a, 2, colCount));
+
+			for (int i = 0; i < rowCount; i++)
+				delete[] a[i];
+			delete[] a;
+		}
+	};
+}
